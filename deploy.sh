17413683f514846442c9648265a144c4c76abcbe
Sebastian Octavian SAS <sebastianoctavian.sas@yahoo.com>
2021-06-29 12:50:14 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+for /f %i in (aws codeartifact list-package-versions --package my-package-name --domain $DOMAIN --domain-owner 043519888587 \
+--repository $REPO --format npm --output text --query 'versions[*].[version]') do set CODEARTIFACT_VERSION = %i
+
+IFS='.' read -r -a versions_array <<< CODEARTIFACT_VERSION
+
+minorVersion =  versions_array[${versions_array[@]}]
+majorVersion =  versions_array[${versions_array[0]}]
+midVersion =  versions_array[${versions_array[1]}]
+
+version = "${majorVersion}.${midVersion}.${minorVersion}"
+dotnet pack -o . -p:version
+dotnet nuget push *.nupkg -s codeartifact
