147e9031e5538b5afa2884e860848e142b222e2c
Sebastian Octavian SAS <sebastianoctavian.sas@yahoo.com>
2021-06-29 16:51:15 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-versions = aws codeartifact list-package-versions --package my-package-name --domain $DOMAIN --domain-owner 043519888587 --repository $REPO --format npm --output text --query 'versions[*].[version]'
+versions=aws codeartifact list-package-versions --package my-package-name --domain $DOMAIN --domain-owner 043519888587 --repository $REPO --format npm --output text --query 'versions[*].[version]'
 
 echo $versions
 
@@ -8,15 +8,15 @@ if [${versions[@]} -eq 0]; then
   dotnet pack -o .
   dotnet nuget push *.nupkg -s codeartifact
 else
-  latest_version = versions[${versions[@]}]
+  latest_version=versions[${versions[@]}]
 
   IFS='.' read -r -a versions_array <<< $latest_version
 
-  minorVersion =  versions_array[${versions_array[@]}]
-  majorVersion =  versions_array[${versions_array[0]}]
-  midVersion =  versions_array[${versions_array[1]}]
+  minorVersion=versions_array[${versions_array[@]}]
+  majorVersion=versions_array[${versions_array[0]}]
+  midVersion= versions_array[${versions_array[1]}]
 
-  version = "${majorVersion}.${midVersion}.${minorVersion}"
+  version="${majorVersion}.${midVersion}.${minorVersion}"
   dotnet pack -o . -p:version
   dotnet nuget push *.nupkg -s codeartifact
 fi
