83aa4c71c7c997add4d6e5b6a53fb26389fec71d
Sebastian Octavian SAS <sebastianoctavian.sas@yahoo.com>
2021-06-29 16:26:27 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for ((/f %i in (aws codeartifact list-package-versions --package my-package-name --domain $DOMAIN --domain-owner 043519888587 --repository $REPO --format npm --output text --query 'versions[*].[version]'))); do
+versions = aws codeartifact list-package-versions --package my-package-name --domain $DOMAIN --domain-owner 043519888587 --repository $REPO --format npm --output text --query 'versions[*].[version]'
+
+for ((/f %i in versions)); do
   set CODEARTIFACT_VERSION = %i
 done
 
