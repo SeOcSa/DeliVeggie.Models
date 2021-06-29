e66c22ed06bcf5120c8837ff168ce6204ebfc9f3
Sebastian Octavian SAS <sebastianoctavian.sas@yahoo.com>
2021-06-29 18:35:22 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@ if [ -z "$versions" ]; then
   dotnet pack -o .
   dotnet nuget push *.nupkg -s codeartifact
 else
-  latest_version=versions[${versions[@]}]
-
+  latest_version=${versions[@]}
   IFS='.' read -r -a versions_array <<< $latest_version
 
-  minorVersionValue=versions_array[2]
+  minorVersionValue=${versions_array[2]}
   ((minorVersionValue++))
-  minorVersion=minorVersionValue
-  majorVersion=versions_array[${versions_array[0]}]
-  midVersion=versions_array[${versions_array[1]}]
+  minorVersion=$minorVersionValue
+  majorVersion=${versions_array[0]}
+  midVersion=${versions_array[1]}
 
   version="$majorVersion.$midVersion.$minorVersion"
   echo $version
+
   dotnet pack -o . -p:$version
   dotnet nuget push *.nupkg -s codeartifact
 fi
